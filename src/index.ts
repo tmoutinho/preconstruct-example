7d727e0024f8d005591fa4d7e797eca710e752bd
Tiago Moutinho <tiagoagm@gmail.com>
2022-08-11 23:09:07 +0100

    Typescript examples cases

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
+type Human = {
+  name: string;
+};
+
+type Robot = {
+  designation: string;
+  status: "idle" | "working";
+};
+
+type Information<T, K extends keyof T, V> = {
+  [P in K]: V;
+};
+
+// if type is Human, then return Human, otherwise return never
+type GetRequiredInformation<TObj> = TObj extends Human ? Human : never;
+
+type GetRequiredInformationForHumam = GetRequiredInformation<Human>; // { name: string }
+type GetRequiredInformationForRobot = GetRequiredInformation<Robot>; // never
+type GetRequiredInformationForNothing = GetRequiredInformation<{}>; // never
+
+type Cases = [
+  Information<Human, "name", "test">, // { name: "test" }
+  Information<Robot, "designation", "test"> // { designation: "test" }
+];
+
 export const Hello = () => {
   return null;
 };
